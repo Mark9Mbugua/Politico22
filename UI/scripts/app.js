fcef37548795db4a6587d09c5867d945a4ba498e
Mark Mbugua <mbuguamark@gmail.com>
2019-03-20 08:48:26 +0300

    [Starts #164310456] create user sign in request

---
+++ UI/scripts/app.js
@@ -0,0 +1,110 @@
+const BASE_URL = 'https://politico-final.herokuapp.com/api/v2';
+var office_ids = [];
+
+function getToken(){
+    token = localStorage.getItem('token');
+    if (token)
+        return token;
+    window.location.replace('sign_up.html')
+    return null
+}
+
+function createNode(type, id, clazz){
+    const node = document.createElement(type);
+    node.classList.add(clazz)
+    node.id = id;
+    return node;
+}
+
+function showModal(modal_id){
+    document.getElementById(modal_id).style.display='block';
+}
+
+function tokenError(status){
+    if(status === 401){
+        window.location.replace('sign_up.html')
+        return true;
+    }
+    return false;
+}
+
+function displayError(msg){
+    document.getElementById('loggy').innerText = msg
+    document.getElementById('loggy').style.backgroundColor = '#d32f2f';
+    showLoggy();
+}
+
+function displaySuccess(msg){
+    document.getElementById('loggy').innerText = msg
+    document.getElementById('loggy').style.backgroundColor = '#1abc9c';
+    showLoggy();
+}
+
+function displayInfo(msg){
+    document.getElementById('loggy').innerText = msg
+    document.getElementById('loggy').style.backgroundColor = '#2980b9';
+    showLoggy();
+}
+
+function showLoggy(){
+    var x = document.getElementById('loggy');
+    x.className = "show";
+    setTimeout(function(){x.className = x.className.replace("show", ""); }, 3000);
+
+}
+
+/**
+ * Login function
+ */
+ 
+ function onLogin() {
+     loader = document.getElementById('load-modal');
+     //loader.style.display = 'block';
+
+     fetch(`${BASE_URL}/auth/signin`, {
+         method: 'POST',
+         headers: {
+             'Content-Type': 'application/json',
+         },
+         body: JSON.stringify({
+             username: document.getElementById('login-username').value,
+             password: document.getElementById('login-password').value,
+
+         }),
+     })
+     .then(res => res.json())
+     .then((data) => {
+         //loader.style.display = 'none';
+
+         if (data.status === 201) {
+             console.log(data.user[0]);
+             var user = data.user[0];
+
+            //Save user profile to local storage
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('firstname', user.firstname);
+            localStorage.setItem('lastname', user.lastname);
+            localStorage.setItem('username', user.username);
+            localStorage.setItem('email', user.email);
+            localStorage.setItem('phone', user.phone);
+            localStorage.setItem('admin', user.is_admin);
+            localStorage.setItem('user_id', user.user_id);
+            
+            //Redirect to homepage after successful login
+            if (user.is_admin === true){
+                window.location.replace('admin_dashboard.html');
+            }else{
+                window.location.replace('user_profile.html');
+            }
+            
+         }else {
+             displayError(data.error)
+             console.log(data.status);
+         }
+     })
+     .catch((error) => {
+        console.log(error);
+        //loader.style.display = 'none';
+        displayError('Kindly check your connection');
+     });
+}
